d455213d82aa84702f70b9c74e7092cd227bf261
系统运维管理部-何彦柱 <heyanzhu@dongling.com.cn>
2017-05-02 14:41:55 +0800

    20170502

--- doc/createThread.md
+++ doc/createThread.md
@@ -1,1 +1,3 @@
 # 创建线程
+## 继承Thread,重新run方法 ==> <a href="https://github.com/kengJ/Thread/blob/master/src/main/java/com/Thread/Create/CreateThread1.java">demo</a>
+
--- src/main/java/com/Thread/Create/CreateThread1.java
+++ src/main/java/com/Thread/Create/CreateThread1.java
@@ -1,7 +1,18 @@
 package com.Thread.Create;
 
-public class CreateThread1 extends new Runnable() {
-	public void run() {
-		
+/**
+*通过继承Thread类实现多线程
+**/
+public class CreateThread1 extends Thread{
+
+	private int i = 0;
+
+	/**
+	*重写run方法
+	**/
+	public void run(){
+		for(i=0;i<100;i++){
+			System.out.print(Thread.currentThread().getName() + " " + i);
+		}
 	}
 }
